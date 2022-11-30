5e6f17d583db0cbd8aeb3275e581413ca117a5b5
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-11-30 15:37:59 +0300

    Add styles + adaptive + autoformat by vscode

--- index.php
+++ index.php
@@ -1,37 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="./assets/css/main.css" rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
 
+<body>
     <?php if (isset($_POST['title'])) { ?>
         <section>
-            <?php require_once (__DIR__ . '/core.php') ?>
+            <?php require_once(__DIR__ . '/core.php') ?>
         </section>
     <? } ?>
-    
+
     <section>
         <div class="container">
             <div class="row">
                 <div class="col">
 
                     <form action="/" method="POST" enctype='multipart/form-data'>
-                        
+
                         <h2>Новая заявка</h2>
-                        <div class="form_item">
+                        <div class="form_item application">
                             <label for="title">Заголовок заявки</label>
                             <input type="text" name="title" required>
                         </div>
-                        
+
                         <h2>Категория</h2>
-                        <div class="form_item">
+                        <div class="form_item category">
                             <label>
-                                <input 
+                                <input
                                     class="form-check-input mt-0"
                                     type="radio"
                                     value="consumables"
@@ -41,7 +43,7 @@
                             </label>
 
                             <label>
-                                <input 
+                                <input
                                     class="form-check-input mt-0"
                                     type="radio"
                                     value="wheels"
@@ -49,11 +51,11 @@
                                 <span>Шины, диски</span>
                             </label>
                         </div>
-                        
+
                         <h2>Вид заявки</h2>
                         <div class="form_item">
                             <label>
-                                <input 
+                                <input
                                     class="form-check-input mt-0"
                                     type="radio"
                                     value="price_delivery"
@@ -63,16 +65,16 @@
                             </label>
 
                             <label>
-                                <input 
+                                <input
                                     class="form-check-input mt-0"
                                     type="radio"
                                     value="replenishment"
                                     name="type">
                                 <span>Пополнение складов</span>
                             </label>
-                            
+
                             <label>
-                                <input 
+                                <input
                                     class="form-check-input mt-0"
                                     type="radio"
                                     value="special_orders"
@@ -82,7 +84,7 @@
                         </div>
 
                         <h2>Склад поставки</h2>
-                        <div class="form_item">
+                        <div class="form_item delivery_warehouse">
                             <select name="stock" id="stock">
                                 <option disabled selected value> -- Выберите склад поставки -- </option>
                                 <option value="1">Склад 1</option>
@@ -92,8 +94,8 @@
                         </div>
 
                         <h2>Состав заявки</h2>
-                        <div class="form_item">
-                            
+                        <div class="form_item structure_application">
+
                             <div class="comp_head">
                                 <span>Бренд</span>
                                 <span>Наименование</span>
@@ -101,7 +103,7 @@
                                 <span>Фасовка</span>
                                 <span>Клиент</span>
                             </div>
-                            
+
                             <div id="comp_rows">
                                 <div class="comp_row">
                                     <select name="brand">
@@ -122,17 +124,17 @@
                                 <button type="button" id="row_minus">-</button>
                             </div>
                         </div>
-                        
+
                         <div class="form_item">
-                            <input type="file" name="upload_file">    
+                            <input type="file" name="upload_file">
                         </div>
-                        
+
                         <div class="form_item">
                             <p>Комментарий</p>
                             <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
                         </div>
-                        
-                        <div class="form_item">
+
+                        <div class="form_item submit">
                             <input type="submit" value="Отправить">
                         </div>
 
@@ -146,4 +148,5 @@
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="/assets/js/index.js"></script>
 </body>
+
 </html>
